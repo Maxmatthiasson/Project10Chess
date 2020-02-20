a0aa5b35604abcf649231a45f4d02e796f0784f1
Filip <filip.spanberg@gmail.com>
2020-02-20 11:28:32 +0100

    Added pawn test case

---
+++ test/logic/ChessGameTest.java
@@ -0,0 +1,22 @@
+package logic;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChessGameTest {
+
+    @org.junit.jupiter.api.Test
+    void movePiece() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getNonCapturedPieceAtLocation() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isNonCapturedPieceAtLocation() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void inCheck() {
+    }
+}
---
+++ test/logic/MoveValidatorTest.java
@@ -0,0 +1,185 @@
+package logic;
+
+import enums.Color;
+import enums.Type;
+
+import java.util.LinkedList;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MoveValidatorTest {
+
+    // Test white pawn moves
+    @org.junit.jupiter.api.Test
+    void isMoveValidWhitePawnOneStep() {
+        LinkedList<Piece> pieces = new LinkedList<>();
+        pieces.add(new Piece(Color.WHITE, Type.PAWN, 1, 2));
+        ChessGame game = new ChessGame(pieces);
+        MoveValidator validator = new MoveValidator(game);
+        assertTrue(validator.isMoveValid(1, 2, 2, 2));
+    }
+
+    @org.junit.jupiter.api.Test
+    void isMoveValidWhitePawnTwoStep() {
+        LinkedList<Piece> pieces = new LinkedList<>();
+        pieces.add(new Piece(Color.WHITE, Type.PAWN, 1, 2));
+        ChessGame game = new ChessGame(pieces);
+        MoveValidator validator = new MoveValidator(game);
+        assertTrue(validator.isMoveValid(1, 2, 3, 2));
+    }
+
+    @org.junit.jupiter.api.Test
+    void isMoveValidWhitePawnOneStepOnOpponent() {
+        LinkedList<Piece> pieces = new LinkedList<>();
+        pieces.add(new Piece(Color.WHITE, Type.PAWN, 1, 2));
+        pieces.add(new Piece(Color.BLACK, Type.PAWN, 2, 2));
+        ChessGame game = new ChessGame(pieces);
+        MoveValidator validator = new MoveValidator(game);
+        assertFalse(validator.isMoveValid(1, 2, 2, 2));
+    }
+
+    @org.junit.jupiter.api.Test
+    void isMoveValidWhitePawnTwoStepOverOpponent() {
+        LinkedList<Piece> pieces = new LinkedList<>();
+        pieces.add(new Piece(Color.WHITE, Type.PAWN, 1, 2));
+        pieces.add(new Piece(Color.BLACK, Type.PAWN, 2, 2));
+        ChessGame game = new ChessGame(pieces);
+        MoveValidator validator = new MoveValidator(game);
+        assertFalse(validator.isMoveValid(1, 2, 3, 2));
+    }
+
+    @org.junit.jupiter.api.Test
+    void isMoveValidWhitePawnBackwards() {
+        LinkedList<Piece> pieces = new LinkedList<>();
+        pieces.add(new Piece(Color.WHITE, Type.PAWN, 1, 2));
+        ChessGame game = new ChessGame(pieces);
+        MoveValidator validator = new MoveValidator(game);
+        assertFalse(validator.isMoveValid(1, 2, 1, 2));
+    }
+
+    @org.junit.jupiter.api.Test
+    void isMoveValidWhitePawnDiagonalNoCapture() {
+        LinkedList<Piece> pieces = new LinkedList<>();
+        pieces.add(new Piece(Color.WHITE, Type.PAWN, 1, 2));
+        ChessGame game = new ChessGame(pieces);
+        MoveValidator validator = new MoveValidator(game);
+        assertFalse(validator.isMoveValid(1, 2, 2, 3));
+    }
+
+    @org.junit.jupiter.api.Test
+    void isMoveValidWhitePawnDiagonalCapture() {
+        LinkedList<Piece> pieces = new LinkedList<>();
+        pieces.add(new Piece(Color.WHITE, Type.PAWN, 1, 2));
+        Piece capture = new Piece(Color.BLACK, Type.PAWN, 2, 3);
+        pieces.add(capture);
+        ChessGame game = new ChessGame(pieces);
+        MoveValidator validator = new MoveValidator(game);
+        assertTrue(validator.isMoveValid(1, 2, 2, 3));
+    }
+
+    // Black pawn moves (game starts for white, so white must make the first move)
+    @org.junit.jupiter.api.Test
+    void isMoveValidBlackPawnOneStep() {
+        LinkedList<Piece> pieces = new LinkedList<>();
+        pieces.add(new Piece(Color.BLACK, Type.PAWN, 6, 2));
+        pieces.add(new Piece(Color.WHITE, Type.PAWN, 1, 2));
+        pieces.add(new Piece(Color.BLACK, Type.KING, 7, 7));
+        pieces.add(new Piece(Color.WHITE, Type.KING, 0, 0));
+        ChessGame game = new ChessGame(pieces);
+        MoveValidator validator = new MoveValidator(game);
+        game.movePiece(1, 2, 2, 2);
+        assertTrue(validator.isMoveValid(6, 2, 5, 2));
+    }
+
+    @org.junit.jupiter.api.Test
+    void isMoveValidBlackPawnTwoStep() {
+        LinkedList<Piece> pieces = new LinkedList<>();
+        pieces.add(new Piece(Color.BLACK, Type.PAWN, 6, 2));
+        pieces.add(new Piece(Color.WHITE, Type.PAWN, 1, 2));
+        pieces.add(new Piece(Color.BLACK, Type.KING, 7, 7));
+        pieces.add(new Piece(Color.WHITE, Type.KING, 0, 0));
+        ChessGame game = new ChessGame(pieces);
+        MoveValidator validator = new MoveValidator(game);
+        game.movePiece(1, 2, 2, 2);
+        assertTrue(validator.isMoveValid(6, 2, 4, 2));
+    }
+
+
+    @org.junit.jupiter.api.Test
+    void isMoveValidBlackPawnOneStepOnOpponent() {
+        LinkedList<Piece> pieces = new LinkedList<>();
+        pieces.add(new Piece(Color.BLACK, Type.PAWN, 6, 2));
+        pieces.add(new Piece(Color.WHITE, Type.PAWN, 4, 2));
+        pieces.add(new Piece(Color.BLACK, Type.KING, 7, 7));
+        pieces.add(new Piece(Color.WHITE, Type.KING, 0, 0));
+        ChessGame game = new ChessGame(pieces);
+        MoveValidator validator = new MoveValidator(game);
+        game.movePiece(4, 2, 5, 2);
+        assertFalse(validator.isMoveValid(6, 2, 5, 2));
+    }
+
+    @org.junit.jupiter.api.Test
+    void isMoveValidBlackPawnTwoStepOverOpponent() {
+        LinkedList<Piece> pieces = new LinkedList<>();
+        pieces.add(new Piece(Color.BLACK, Type.PAWN, 6, 2));
+        pieces.add(new Piece(Color.WHITE, Type.PAWN, 4, 2));
+        pieces.add(new Piece(Color.BLACK, Type.KING, 7, 7));
+        pieces.add(new Piece(Color.WHITE, Type.KING, 0, 0));
+        ChessGame game = new ChessGame(pieces);
+        MoveValidator validator = new MoveValidator(game);
+        game.movePiece(4, 2, 5, 2);
+        assertFalse(validator.isMoveValid(6, 2, 4, 2));
+    }
+
+    @org.junit.jupiter.api.Test
+    void isMoveValidBlackPawnBackwards() {
+        LinkedList<Piece> pieces = new LinkedList<>();
+        pieces.add(new Piece(Color.BLACK, Type.PAWN, 6, 2));
+        pieces.add(new Piece(Color.WHITE, Type.PAWN, 1, 2));
+        pieces.add(new Piece(Color.BLACK, Type.KING, 7, 7));
+        pieces.add(new Piece(Color.WHITE, Type.KING, 0, 0));
+        ChessGame game = new ChessGame(pieces);
+        MoveValidator validator = new MoveValidator(game);
+        game.movePiece(1, 2, 2, 2);
+        assertFalse(validator.isMoveValid(6, 2, 7, 2));
+    }
+
+    @org.junit.jupiter.api.Test
+    void isMoveValidBlackPawnDiagonalNoCapture() {
+        LinkedList<Piece> pieces = new LinkedList<>();
+        pieces.add(new Piece(Color.BLACK, Type.PAWN, 6, 2));
+        pieces.add(new Piece(Color.WHITE, Type.PAWN, 1, 2));
+        pieces.add(new Piece(Color.BLACK, Type.KING, 7, 7));
+        pieces.add(new Piece(Color.WHITE, Type.KING, 0, 0));
+        ChessGame game = new ChessGame(pieces);
+        MoveValidator validator = new MoveValidator(game);
+        game.movePiece(1, 2, 2, 2);
+        assertFalse(validator.isMoveValid(6, 2, 5, 3));
+    }
+
+    @org.junit.jupiter.api.Test
+    void isMoveValidBlackPawnDiagonalCapture() {
+        LinkedList<Piece> pieces = new LinkedList<>();
+        pieces.add(new Piece(Color.BLACK, Type.PAWN, 6, 2));
+        Piece capture = new Piece(Color.WHITE, Type.PAWN, 4, 3);
+        pieces.add(capture);
+        pieces.add(new Piece(Color.BLACK, Type.KING, 7, 7));
+        pieces.add(new Piece(Color.WHITE, Type.KING, 0, 0));
+        ChessGame game = new ChessGame(pieces);
+        MoveValidator validator = new MoveValidator(game);
+        game.movePiece(4, 3, 5, 3);
+        assertTrue(validator.isMoveValid(6, 2, 5, 3));
+    }
+
+    
+
+    @org.junit.jupiter.api.Test
+    void checkValidator() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void mateValidator() {
+    }
+}
